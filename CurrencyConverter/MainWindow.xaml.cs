10fa8687de468a8f8e5fc8633674d26508242e1d
adyshkiss@outlook.com <adyshkiss@outlook.com>
2020-10-28 23:37:11 +0300

    Ничего не получилось

--- CurrencyConverter/MainWindow.xaml.cs
+++ CurrencyConverter/MainWindow.xaml.cs
@@ -31,5 +31,20 @@ namespace CurrencyConverter
         {
             Application.Current.Shutdown();
         }
+
+
+        //Парсинг данных 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.Net.WebClient webClient = new System.Net.WebClient();
+            String Respons = webClient.DownloadString("https://moskva.bankiros.ru/currency");
+            // span id="u">79.09</span>
+            usdCB.Text = Respons;//System.Text.RegularExpressions.Regex.Match(Respons, @"(<td class=""mark - text"">)([0-9]+\.[0-9]+)</td>").Groups[1].Value;
+
+            usdPurchase.Text = System.Text.RegularExpressions.Regex.Match(Respons, @": block"">([0-9]+\.[0-9]+)</span>").Groups[1].Value;
+            //usdCB.Text = System.Text.RegularExpressions.Regex.Match(Respons, @">([0-9]+\.[0-9]+)76,46</div>").Value;
+
+            // ([0-9]+\.[0-9]+)
+        }
     }
 }
